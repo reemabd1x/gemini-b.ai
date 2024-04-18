a7243f1e4477baeea0025baaff614b40eb1cc3a5
reemabd1x <156617298+reemabd1x@users.noreply.github.com>
2024-04-18 09:30:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded())
 app.get('/', (req, res) => {
-    res.json({ run: 'run bot1' })
+    res.json({ run: 'run bot2' })
 }); app.listen(process.env.PORT || 3000, () => { console.log(`listen`) })
 
 // PING BOT ----
