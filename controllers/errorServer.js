d055410a3ce252f536c6b520504ab6cee71af68b
reemabd1x <156617298+reemabd1x@users.noreply.github.com>
2024-10-22 09:52:39 +0300

    Update errorServer.js

--- controllers/errorServer.js
+++ controllers/errorServer.js
@@ -1,12 +1,12 @@
-// export const errorServer = async (e, t) => {
-//   const ET = `${e}: ${t}`
-//     try {
-//       const formData = new FormData(); formData.append("text", ET);
-//       const tokin = '6351210996:AAEqIL8M169m5qfnS2qCz2VXKilvqFT9WMM'
-//       const res = await fetch(`https://api.telegram.org/bot${tokin}/sendMessage?chat_id=5358365084`, {
-//         method: 'POST',
-//         body: formData
-//       })
-//     } catch (err) { console.log('err') }
-//   }
+export const errorServer = async (e, t) => {
+  const ET = `${e}: ${t}`
+    try {
+      const formData = new FormData(); formData.append("text", ET);
+      const tokin = '6351210996:AAEqIL8M169m5qfnS2qCz2VXKilvqFT9WMM'
+      const res = await fetch(`https://api.telegram.org/bot${tokin}/sendMessage?chat_id=5358365084`, {
+        method: 'POST',
+        body: formData
+      })
+    } catch (err) { console.log(err) }
+  }
 
