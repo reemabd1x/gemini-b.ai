3e83a9fe5f7b78e9b398eecbc1662df200a5765d
reemabd1x <156617298+reemabd1x@users.noreply.github.com>
2024-10-30 11:34:02 +0300

    Update chatMember.js

--- controllers/chatMember.js
+++ controllers/chatMember.js
@@ -5,11 +5,11 @@ export const chatMember = async (bot, id) => {
         if (result.status === 'left') {
             bot.sendMessage(id, 'عذرا يجب عليك اولا الأشتراك بالقناة, Sorry, you must subscribe to the channel first.', {
                 'reply_markup': { "inline_keyboard": [[{ text: "اشترك من هنا", url: "https://t.me/xaztom_channel" }]] }
-            }); return false
+            }); return true
         } else if (result.status === 'kicked') {
             bot.sendMessage(id, 'انت محضور من الأستخدام, راجع المنشئ لمساعدتك, You are banned from use.  Contact the creator for assistance.', {
                 'reply_markup': { "inline_keyboard": [[{ text: "Bashar", url: "https://t.me/bashar1_x" }]] }
-            }); return false
-        } else return true
+            }); return true
+        } else return false
     } catch (err) { console.log(err) }
 }
