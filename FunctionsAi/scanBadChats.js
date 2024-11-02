d56a6b59c35d78cead4731ead03b9c96efa7b2ea
reemabd1x <156617298+reemabd1x@users.noreply.github.com>
2024-11-02 22:10:29 +0300

    Update scanBadChats.js

--- FunctionsAi/scanBadChats.js
+++ FunctionsAi/scanBadChats.js
@@ -15,5 +15,5 @@ export const scanBadChats = async (text, file) => {
     } catch (err) { console.log(err); return 'false' }
 };
 
-
-const directions = `You will act as an inspector for harmful messages such as insults, abuse, disrespect to others, mockery, incitement to hatred, and belittling others, Open topics related to politics or terrorism, such as ISIS. You will be sent a text message and possibly an image. Examine if there is anything harmful. If there is, write "true", otherwise write "false".`
+const directions = `You will act as an inspector for harmful messages, such as insults, political topics, and especially ISIS-related content. A text message may be sent to you, possibly accompanied by an image. Review it to see if there is any harmful content. If there is, write "true"; if there isn't, write "false".`
+// const directions = `You will act as an inspector for harmful messages such as insults, abuse, disrespect to others, mockery, incitement to hatred, and belittling others, Open topics related to politics or terrorism, such as ISIS. You will be sent a text message and possibly an image. Examine if there is anything harmful. If there is, write "true", otherwise write "false".`
