7180aff796c1864a20d469976e074f924d57c190
reemabd1x <156617298+reemabd1x@users.noreply.github.com>
2025-08-11 08:07:54 +0300

    Update runChat.js

--- FunctionsAi/runChat.js
+++ FunctionsAi/runChat.js
@@ -6,13 +6,15 @@ dotenv.config();
 const genAI = new GoogleGenerativeAI(process.env.KEY_XAZTOM);
 export const runChat = async (text, nameUser, file) => {
     console.log('run', text);
-    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-exp", safetySettings, systemInstruction: systemInstruction(nameUser) })
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash", safetySettings, systemInstruction: systemInstruction(nameUser) })
     try {
         const chat = model.startChat({ generationConfig, })
         const result = file ? await chat.sendMessage([file, text]) : await chat.sendMessage(text)
         const response = result.response.text()
         return response
-    } catch (err) { console.log(err); return errorMessages[Math.floor(Math.random() * errorMessages.length - 1) + 1] }
+    } catch (err) {
+        console.log(err)
+        return err }
 }
 
 const errorMessages = [
@@ -21,5 +23,6 @@ const errorMessages = [
     "An error occurred.  Try again later.",
     "Failed to process request. Please try again."
 ];
+
 
 
